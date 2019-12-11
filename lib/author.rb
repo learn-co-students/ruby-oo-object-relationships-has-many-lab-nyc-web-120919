22a3532a675cba5ac9b84e1f4afb0f0b0d3a6757
merewelch214 <merewelch@gmail.com>
2019-12-11 16:29:38 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+    attr_accessor :name, :posts, :title
+
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        Post.all.select {|post| post.author == self}
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        new_post = Post.new(title)
+        add_post(new_post)
+    end
+
+    def self.post_count
+        Post.all.length
+    end
+
+end
