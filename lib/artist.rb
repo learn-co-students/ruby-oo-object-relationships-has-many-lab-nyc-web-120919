cf564247c2b4c92935dd0327560439b2919a2121
Ben Kagan <kagan.benjamin@gmail.com>
2019-12-16 18:00:51 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+attr_accessor :name
+@@all = [] 
+
+    def initialize(name)
+    @name = name 
+    @songs = [] 
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self  
+        end
+    end
+    
+    def add_song(song)  
+        song.artist = self 
+    end
+
+    def add_song_by_name(song)
+        new_song = Song.new(song) 
+        new_song.artist = self 
+    end
+
+    def self.song_count
+        Song.all.count 
+    end
+end
